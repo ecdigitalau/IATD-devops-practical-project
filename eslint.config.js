38c301e34d66e78dfc4ade3122dc5062952b4e1b
Ernest Thomas <ernest@ecdigital.com.au>
2025-06-25 19:45:50 +1000

    removed and added package-lock.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,7 @@
 // eslint.config.js
-// import eslintPluginNode from 'eslint-plugin-node';
-
-// export default [
-//   {
-//     ignores: ['node_modules/**'],
-//     files: ['**/*.js'],
-//     languageOptions: {
-//       ecmaVersion: 2022,
-//       sourceType: 'module',
-//     },
-//     plugins: {
-//       node: eslintPluginNode,
-//     },
-//     rules: {
-//       'no-unused-vars': 'warn',
-//       'no-undef': 'error',
-//       'semi': ['error', 'always'],
-//     },
-//   },
-// ];
-
-// eslint.config.js
 import eslintPluginNode from 'eslint-plugin-node';
-import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
+export default [
   {
     ignores: ['node_modules/**'],
     files: ['**/*.js'],
@@ -35,14 +12,11 @@ export default defineConfig([
     plugins: {
       node: eslintPluginNode,
     },
-    // If you want to use the plugin's recommended config:
-    extends: [eslintPluginNode.configs.recommended],
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'error',
       'semi': ['error', 'always'],
-      // You can add plugin rules like:
-      // 'node/no-unsupported-features/es-syntax': 'error',
     },
   },
-]);
+];
+
